606323b062a7451caa0daac2b9f24276f1e1982b
Firofa <rizifaarsien@gmail.com>
2023-01-29 11:32:49 +0700

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>Arsien's Foodary &copy; 2023</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,31 @@
+export const my_recipe = {
+  recipe_1: {
+    name: "Chirashi Don",
+    image: "don.jpg",
+    preparation_time: "30 minutes",
+    servings: "2",
+    ingredients: [
+      "1/2 cup rice",
+      "1/2 cup water",
+      "2 eggs",
+      "300g of salmon, tuna and salmon roe",
+    ],
+    instructions: [
+      "Cook rice and water in a rice cooker",
+      "Fry egg in a pan",
+      "Serve rice, eggs and fish on a plate",
+    ],
+  },
+  recipe_2: {
+    name: "Simple Fried Egg",
+    image: "egg.jpg",
+    preparation_time: "5 minutes",
+    servings: "1",
+    ingredients: ["1 eggs", "Cooking oil or butter to taste", "salt to taste"],
+    instructions: [
+      "Cook egg with cooking oil or butter",
+      "Add salt to taste",
+      "Serve it on a plate",
+    ],
+  },
+};
